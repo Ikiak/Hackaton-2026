44ed411961fd6c632d521f07733f81d79b3a30fa
ArthurSGO <arthursg33@gmail.com>
2026-06-20 15:09:05 -0300

    chore: cria projeto backend com Spring Boot

---
+++ backend/src/test/java/br/com/bolao/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bolao.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
